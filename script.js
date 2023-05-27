620e2fbc2f29983dcc486d569f633c66f78a8404
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-05-27 19:54:33 -0300

    19.54

--- script.js
+++ script.js
@@ -1,52 +1,23 @@
-// Función para obtener una respuesta aleatoria
-function obtenerRespuestaAleatoria() {
-    const respuestas = [
-        "Es cierto.",
-        "Es decididamente así.",
-        "Sin duda.",
-        "Sí, definitivamente.",
-        "Puedes contar con ello.",
-        "Según mi punto de vista, sí.",
-        "Muy probable.",
-        "Las perspectivas son buenas.",
-        "Sí.",
-        "Todo apunta a que sí.",
-        "Es confuso, intenta de nuevo.",
-        "Pregunta de nuevo más tarde.",
-        "Mejor no decirte ahora.",
-        "No se puede predecir ahora.",
-        "Concéntrate y pregunta de nuevo.",
-        "No cuentes con ello.",
-        "Mi respuesta es no.",
-        "Mis fuentes dicen que no.",
-        "Las perspectivas no son buenas.",
-        "Muy dudoso."
-    ];
-
-    const indiceAleatorio = Math.floor(Math.random() * respuestas.length);
-    return respuestas[indiceAleatorio];
-}
-
-// Función para mostrar la respuesta en pantalla
-function mostrarRespuesta(respuesta) {
-    const respuestaElemento = document.getElementById("respuesta");
-    respuestaElemento.textContent = respuesta;
-}
-
-// Función para manejar el evento de agitar la Magic 8 Ball
-function agitarMagic8Ball() {
-    const respuesta = obtenerRespuestaAleatoria();
-    mostrarRespuesta(respuesta);
-}
-
-// Evento de agitar la Magic 8 Ball al hacer clic en el botón
-document.getElementById("boton-agitar").addEventListener("click", agitarMagic8Ball);
-
-// Evento de agitar la Magic 8 Ball al mover el dispositivo en la versión móvil
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
-        if (event.beta > 20 || event.beta < -20 || event.gamma > 20 || event.gamma < -20) {
-            agitarMagic8Ball();
-        }
-    });
-}
+$(document).ready(function() {
+    var Magic8Ball = {};
+  
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+  
+    Magic8Ball.respuestas = ["No", "Podría ser", "Es posible", "Intenta de nuevo", "Tú puedes lograrlo", "Podría ser un poco difícil", "Deberías pensarlo"];
+  
+    Magic8Ball.pregunta = function() {
+      var indiceAleatorio = Math.floor(Math.random() * this.respuestas.length);
+      var respuesta = this.respuestas[indiceAleatorio];
+      $("#answer").text(respuesta);
+    }
+  
+    var truco = function() {
+      Magic8Ball.pregunta();
+      prompt("Haz una pregunta que pueda ser respondida con SÍ o NO");
+      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+      $("#8ball").effect("shake");
+    }
+  
+    $("#questionButton").click(truco);
+  });
+  
